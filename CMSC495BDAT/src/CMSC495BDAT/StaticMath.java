7f3ee5427f00c313fcaa0c8528b0be427f5da0c1
aritterm08 <60801753+aritterm08@users.noreply.github.com>
2020-02-22 13:03:45 -0500

    Update StaticMath.java
    Updated R-Squared Calculation. Best Fit Curve and P Value are not yet implemented.

--- CMSC495BDAT/src/CMSC495BDAT/StaticMath.java
+++ CMSC495BDAT/src/CMSC495BDAT/StaticMath.java
@@ -4,7 +4,6 @@ package CMSC495BDAT;
     Author: Adam Rittermann
     Date: 17 February 2020
     Purpose:  Statistics Calculations */
-    
 import java.util.Arrays;
 
 /* Public Methods
@@ -12,20 +11,21 @@ import java.util.Arrays;
     calculateMedian(double[] column): double median
     calculateMode(double[] column): double mode
     calculateStdDeviation(double[] column): double standard deviation
-    calculatePValue(): double pValue
+    calculatePValue() -- NOT IMPLEMENTED
     calculateBestFitLine(double[][] columns): double[] {m,b}
-    calculateBestFitCurve()
-    calculateR2()
+    calculateBestFitCurve() -- NOT IMPLEMENTED
+    calculateR2(double[][] columns): double r2
  */
 public class StaticMath {
 
     private double pValue;
-    private double[] bestFitLine;
     private double bestFitCurve;
-    private double r2;
 
-    /*
-    Calculate Mean of Target Column
+    /**
+     * Calculate Mean of Target Column Values
+     *
+     * @param column double[] column data
+     * @return double mean
      */
     public double calculateMean(double[] column) {
         double sum = 0.0;
@@ -36,8 +36,11 @@ public class StaticMath {
         return ((double) sum / (double) column.length);
     }
 
-    /*
-    Calculate Median of Target Column
+    /**
+     * Calculate Median of Target Column Values
+     * 
+     * @param column double[] column data
+     * @return double median
      */
     public double calculateMedian(double[] column) {
         Arrays.sort(column);
@@ -49,8 +52,11 @@ public class StaticMath {
                 + column[column.length / 2]) / 2.0;
     }
 
-    /*
-    Calculate Mode of Target Column
+    /**
+     * Calculate Mode of Target Column
+     * 
+     * @param column double[] column data
+     * @return double mode
      */
     public double calculateMode(double[] column) {
         int maxCount = 0;
@@ -71,8 +77,11 @@ public class StaticMath {
         return mode;
     }
 
-    /*
-    Calculate Standard Deviation of Target Column
+    /**
+     * Calculate Standard Deviation of Target Column
+     * 
+     * @param column double[] column data
+     * @return double standard deviation
      */
     public double calculateStdDeviation(double[] column) {
         double stdDevSum = 0.0;
@@ -84,17 +93,24 @@ public class StaticMath {
         return Math.sqrt(stdDevSum / column.length);
     }
 
-    /*
-    Calculate PValue of Target Column
+    /**
+     * -- NOT IMPLEMENTED
+     * Calculate PValue of Target Column
+     * 
+     * @param column double[] column data
+     * @return double pValue
+     * -- NOT IMPLEMENTED
      */
     public double calculatePValue(double[] column) {
         return pValue;
     }
 
-    /*
-    Calculate Best Fit Line of Target Columns
-    Maximum 2 columns for X & Y
-    Returns double[] containing m and c for y=mx+b
+    /**
+     * Calculate Best Fit Line of Target Columns
+     * Maximum 2 columns for X & Y
+     * 
+     * @params columns double[][] column x and y
+     * @return double[] m and c for linear function y=mx+b
      */
     public double[] calculateBestFitLine(double[][] columns) {
         double m, b;
@@ -111,24 +127,64 @@ public class StaticMath {
                 / ((columns.length * sumX2) - (Math.pow(sumX, 2)));
         b = (sumY - m * sumX) / columns.length;
 
+        double[] bestFitLine = new double[2];
         bestFitLine[0] = m;
         bestFitLine[1] = b;
 
         return bestFitLine;
     }
 
-    /*
-    Calculate Best Fit Curve of Target Columns
+    /**
+     * -- NOT IMPLEMENTED
+     * Calculate Best Fit Curve of Target Columns
+     * 
+     * @params columns double[][] columns x and y
+     * @return double bestFitCurve
+     * -- NOT IMPLEMENTED
      */
     public double calculateBestFitCurve(double[][] columns) {
         return bestFitCurve;
     }
 
-    /*
-    Calculate R2 of Target Columns
+    /**
+     * Calculate R-Squared Value for Linear Regression
+     *
+     * @param columns double[][] x and y values for calculation
+     * @return double R-squared value
      */
     public double calculateR2(double[][] columns) {
+        double r2 = 0.0;
+        double[] vars = this.calculateBestFitLine(columns);
+        double slope = vars[0];
+        double intercept = vars[1];
+        double sumY = 0;
+        double[] x = new double[columns.length];
+        double[] y = new double[columns.length];
+
+        // Find sum of Y
+        for (double[] row : columns) {
+            sumY += row[1];
+        }
+
+        // Find average of Y
+        double meanY = sumY / columns.length;
+
+        // Sum of Squares Y
+        double ssY = 0.0;
+        for (int i = 0; i < columns.length; i++) {
+            ssY += (columns[i][1] - meanY) * (columns[i][1] - meanY);
+        }
+
+        // Regression Sum of Squares Y
+        double ssr = 0.0;   
+        for (int i = 0; i < columns.length; i++) {
+            double fit = slope * columns[i][0] + intercept;
+            ssr += (fit - meanY) * (fit - meanY);
+        }
+
+        // Calculate R-Squared
+        r2 = ssr / ssY;
+
         return r2;
     }
-
 }
